a125609ff75833c3552e17229d4ab01be4e300a4
Manas Sahoo <manas_sahoo@outlook.com>
2020-11-03 22:27:44 +0530

    Login Code

--- pages/login.js
+++ pages/login.js
@@ -2,64 +2,51 @@ import React, { useState, useContext } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 import axios from 'axios';
+import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
+
 import { API_BASE_URL, APP_BASE_URL } from '../components/constants/api-config';
 import { authHeader } from '../components/constants/authHeader';
 import UserContext from '../components/contexts/UserContext';
 
 import Meta from '../components/Meta';
 
+const validationSchema = Yup.object().shape({
+    email: Yup
+        .string()
+        .email()
+        .required(),
+    password: Yup
+        .string()
+        .min(4)
+        .max(16)
+        .required(),
+});
+
 export default function Login(props) {
     const { login } = useContext(UserContext);
-
-    const [state, setState] = useState({
-        email: "",
-        password: "",
-        successMessage: null,
-        processing: false
-    })
-
-    const handleChange = (e) => {
-        const { id, value } = e.target
-        setState(prevState => ({
-            ...prevState,
-            [id]: value
-        }))
+    const [error, setError] = useState(null);
+    const initialValues = {
+        email: '',
+        password: '',
+    }
+    const onSubmit = (values, actions) => {
+        handleAuthentication(values, actions);
     }
 
-    const handleSubmitClick = (e) => {
-        e.preventDefault();
-        setState(prevState => ({
-            ...prevState,
-            'processing': true
-        }))
-        const payload = {
-            "email": state.email,
-            "password": state.password,
-        }
-        axios.post(`${API_BASE_URL}/auth/login`, payload)
+    const handleAuthentication = (values, actions) => {
+        axios.post(`${API_BASE_URL}/auth/login`, values)
             .then(function (response) {
                 if (response.status === 200) {
-                    setState(prevState => ({
-                        ...prevState,
-                        'successMessage': 'Login successful. Redirecting to home page..',
-                        'processing': false
-                    }))
                     Router.push('/');
-                    // props.showError(null);
                     localStorage.setItem("token", response.data.accessToken);
                     getCurrentUser();
-                } else if (response.status === 204) {
-                    // props.showError("Username and password do not match");
-                } else {
-                    // props.showError("Username does not exists");
                 }
             })
             .catch(function (error) {
-                console.log(error);
-                setState(prevState => ({
-                    ...prevState,
-                    'processing': false
-                }))
+                setError(" Incorrect email or password. ");
+            }).then(function () {
+                actions.setSubmitting(false);
             });
     }
 
@@ -70,18 +57,14 @@ export default function Login(props) {
             .then(function (response) {
                 if (response.status === 200) {
                     login(response.data);
-                } else if (response.status === 204) {
-                    // props.showError("Username and password do not match");
+                } else if (response.status === 401) {
+                    setError(" Incorrect email or password. ");
                 } else {
                     // props.showError("Username does not exists");
                 }
             })
             .catch(function (error) {
                 console.log(error);
-                setState(prevState => ({
-                    ...prevState,
-                    'processing': false
-                }))
             });
     }
 
@@ -96,56 +79,48 @@ export default function Login(props) {
                 <div className="card shadow  m-auto">
                     <div className="card-body m-4">
                         <h1 className="text-primary text-center">Login</h1>
-                        <form className="my-4">
-                            <div className="form-group">
-                                <label htmlFor="email" className="m-0 font-weight-bold text-black-50">Email address</label>
-                                <input
-                                    type="email"
-                                    className="form-control"
-                                    id="email"
-                                    value={state.email}
-                                    onChange={handleChange} />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password" className="m-0 font-weight-bold text-black-50">Password</label>
-                                <input
-                                    type="password"
-                                    className="form-control"
-                                    id="password"
-                                    value={state.password}
-                                    onChange={handleChange} />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="remember">
-                                    <input className="checkbox" id="remember" type="checkbox" />
-                                    <span className="ml-1">Remember me</span>
-                                </label>
-                                <Link href="/forgot"><a className="float-right">Forgot password?</a></Link>
-                            </div>
-                            <div className="form-group">
-                                {state.processing ?
-                                    <button className="btn btn-primary btn-block" type="button" disabled>
-                                        <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                                        <span className="ml-2">Logging in</span>
-                                    </button> :
-                                    <button
-                                        type="submit"
-                                        className="btn btn-primary btn-block"
-                                        onClick={handleSubmitClick}>Login</button>}
-
-
-                            </div>
-                            <div className="form-group">
-                                <p className="text-muted text-center">Do not have an account?
+                        {error && <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            {error}
+                            <button type="button" class="close" aria-label="Close" onClick={() => setError(null)}>
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>}
+                        <Formik
+                            initialValues={initialValues}
+                            validationSchema={validationSchema}
+                            onSubmit={onSubmit}
+                        >
+                            {formik => {
+                                const { errors, touched, isValid, isSubmitting } = formik;
+                                return (
+                                    <Form>
+                                        <div className="form-group">
+                                            <label htmlFor="email" className="m-0 font-weight-bold text-black-50">Email address</label>
+                                            <Field name="email" type="email" className={`${(errors.email && touched.email) ? "form-control is-invalid" : "form-control "}`} />
+                                            <div className="invalid-feedback">{errors.email && touched.email && errors.email}</div>
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="password" className="m-0 font-weight-bold text-black-50">Password</label>
+                                            <Link href="/forgot"><a className="float-right">Forgot password?</a></Link>
+                                            <Field name="password" type="password" className={`${(errors.password && touched.password) ? "form-control is-invalid" : 'form-control '}`} />
+                                            <div className="invalid-feedback">{errors.password && touched.password && errors.password}</div>
+                                        </div>
+                                        <button type="submit" className="btn btn-primary btn-block my-4" disabled={!isValid || isSubmitting}>
+                                            {isSubmitting ? <div><span className="spinner-border spinner-border-sm text-light" role="status" aria-hidden="true"></span>
+                                                <span className="ml-2">Logging in</span></div> : 'Login'}
+                                        </button>
+                                    </Form>
+                                )
+                            }}
+                        </Formik>
+                        <p className="text-muted text-center">Do not have an account?
                                     <Link href="/signup"><a className="mx-2">Signup</a></Link>
-                                </p>
-                            </div>
-                        </form>
+                        </p>
                     </div>
                 </div>
             </div>
             <div className="col d-none d-sm-block"></div>
-        </div>
+        </div >
     )
 }
 
