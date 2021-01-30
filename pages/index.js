f0d10b387f655e037a74fdd6ec457211e1aff4cd
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2021-01-30 15:07:03 +0530

    Topics and phrases

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,22 @@
-import { Typography, Row, Col, Card } from 'antd';
+import { Card, Layout, Row, Col, List, Avatar, Skeleton, Typography } from 'antd';
+
+import Topics from './topics/index'
 
 const { Title } = Typography;
 const { Meta } = Card;
-
+const { Content } = Layout;
 export default function Home() {
 
-  const categories = [
-    { name: "Family and Relatives", image: 'https://ouch-cdn.icons8.com/preview/90/49614dde-8a22-496a-93f7-9e5fb23c081b.png' },
-    { name: "Days", image: 'https://ouch-cdn.icons8.com/preview/263/59d8ad6b-a9ec-4844-8a1f-c8ca07094043.png' },
-    { name: "Colors", image: 'https://ouch-cdn.icons8.com/preview/164/60ebe30b-5a59-4380-97b3-95b1b62268cd.png' }
-  ];
-
   const imgStyle = {
-    height: '100px',
+    height: '75px',
     width: 'auto',
+    maxWidth: '100%'
   }
 
   return (
     <main>
       <Title level={3}>Learn Telugu</Title>
-      <Row gutter={[16, 16]}>
-        {categories.map((category, key) =>
-          <Col xs={24} sm={8} key={key}>
-            <Card hoverable>            
-            <Row align="middle">
-              <Col span={8}><img style={imgStyle} alt={category.name} src={category.image} /></Col>
-              <Col span={16}><Meta title={category.name} description="10 words" /></Col>
-            </Row>
-            </Card>
-          </Col>
-        )}
-      </Row>
+      <Topics />
     </main>
   )
 }
