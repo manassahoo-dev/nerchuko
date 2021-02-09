d151103cbb3c39ab4d54d8cadbf5691b2f2b80d7
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2021-02-09 22:23:47 +0530

    Removed

--- pages/topics/index.js
+++ pages/topics/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import MobileTopicsIndex from '../../components/mobile/index';
 import DesktopTopicsIndex from '../../components/desktop/index';
 
 const TopicsIndex = (props) => {
