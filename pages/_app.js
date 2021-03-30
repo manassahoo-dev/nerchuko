3913631456b0988949d9adaeefa9965f3b02adfb
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2021-03-30 16:03:46 +0530

    Removed unused imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,10 @@
 import Head from 'next/head';
 import Router from 'next/router';
-import NProgress from 'nprogress'; //nprogress module
-import 'nprogress/nprogress.css'; //styles of nprogress
 import { useState } from 'react';
 import AppLayout from "../components/AppLayout";
 import { screenResolution } from '../components/constants/screenResolution';
 import UserContext from '../components/contexts/UserContext';
 import '../public/styles/antd.less';
-
-//Binding events. 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => { NProgress.done(); });
-Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   const resolution = screenResolution();
