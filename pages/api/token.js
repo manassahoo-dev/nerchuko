20f5c3168fe67db158bddfb5c5948e1bbe3caa39
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2021-04-26 20:28:55 +0530

    remove unused code

--- pages/api/token.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'next-auth/jwt'
-
-const secret = process.env.JWT_SECRET
-
-export function token(req, res) {
-    const token = await jwt.getToken({ req, secret, raw: false });
-    res.send(JSON.stringify(token, null, 2))
-};
